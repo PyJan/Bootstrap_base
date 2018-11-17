2ac315bd1d0e48daea7e87267429c1fd0a078940
PyJan <jan.svarcbach@gmail.com>
2018-11-17 22:40:58 +0100

    Quick form implemented

--- bsform.py
+++ bsform.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template, request, url_for, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, validators, SubmitField 
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'keepitreal'
 boostrap = Bootstrap(app)
 
-@app.route('/')
+class Userform(Form):
+    firstname = StringField('First Name')
+    surname = StringField('Surname')
+    submitme = SubmitField('Register Me')
+
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('base.html')
+    userform = Userform()
+    if userform.validate_on_submit():
+        print(userform.firstname.data, userform.surname.data)
+    return render_template('base.html', userform=userform)
 
 if __name__ == "__main__":
     app.run(debug=True)
