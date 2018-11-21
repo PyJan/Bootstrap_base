d2bc9cc16afa021b84d50c1f0f3691d9908ee06b
PyJan <jan.svarcbach@gmail.com>
2018-11-22 00:39:33 +0100

    basic grid structure

--- vlab.py
+++ vlab.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def main():
-    return 'Vikis Lab'
+    return render_template('vlab.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
