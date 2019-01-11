38afb1447415886d84af3dc2b383994cd8aec768
PyJan <jan.svarcbach@gmail.com>
2019-01-11 17:48:14 +0100

    delete column

--- test/flask-migrate/app.py
+++ test/flask-migrate/app.py
@@ -11,7 +11,6 @@ migrate = Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
-    age = db.Column(db.Integer)
 
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
